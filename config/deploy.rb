99a8ed2af06df2b22071f2a779ffe4d5ae5c8bd0
Jesse Dearing <jesse.dearing@gmail.com>
2011-08-03 20:23:01 -0500

    Updated deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,29 @@
+require 'bundler/capistrano'
+
 set :application, "dearings"
-set :repository,  "git@github.com/jessedearing/dearings.git"
+set :repository,  "git@github.com:jessedearing/dearings.git"
+set :deploy_to, '/var/www/dearings'
+set :user, 'jessed'
+set :use_sudo, false
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "dearin.gs"                          # Your HTTP server, Apache/etc
+role :app, "dearin.gs"                          # This may be the same as your `Web` server
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do
+    run "cd #{current_path} && #{sudo} unicorn_rails -c config/unicorn.rb -E #{rails_env} -D"
+  end
+  task :stop do
+    run "#{sudo} kill -QUIT $(cat #{current_path}/tmp/pids/unicorn.pid)"
+  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{sudo} kill -USR2 $(cat #{current_path}/tmp/pids/unicorn.pid)"
+  end
+  task :create_cache_dirs do
+    run "cd #{current_path} && #{sudo} mkdir tmp/cache"
+    run "cd #{current_path} && #{sudo} mkdir tmp/sessions"
+    run "cd #{current_path} && #{sudo} chown -R www-data:www-data tmp"
+  end
+end
