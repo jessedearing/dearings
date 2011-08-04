faf725e9eb30b4af77effc3e9b1d820e76427cf0
Jesse Dearing <jesse.dearing@gmail.com>
2011-08-04 01:57:06 -0500

    Comments + send back shortened URL on post

--- dearings.rb
+++ dearings.rb
@@ -7,6 +7,7 @@ Mongoid.configure do |config|
   config.master = Mongo::Connection.new.db("dearings")
 end
 
+# Capture some data for stats
 def capture_user_info(request, url_id)
   ci = ClickInstance.new
   ci.referer = request.referer
@@ -23,7 +24,7 @@ def capture_user_info(request, url_id)
   ci.save
 end
 
-def gigo
+def not_valid
   status 400
   "Invalid url"
 end
@@ -31,18 +32,21 @@ end
 get '/:id' do
   begin
     if params[:id] =~ /[0-9a-z]/
+      # to_i(36) will use a radix of 36 when parsing a string
       url = ShortenedUrl.where(:int_id => params[:id].to_i(36)).first
       url.inc(:clicks, 1)
       capture_user_info(request, url.id)
       redirect url.long_url
     else
-      gigo
+      not_valid
     end
   rescue
-    gigo
+    not_valid
   end
 end
 
+# Where the link gets added
+# Secure this path on your webserver
 post '/url/new' do
   su = ShortenedUrl.new
   su.long_url = params[:url]
@@ -53,4 +57,5 @@ post '/url/new' do
     su.int_id = last.int_id + 1
   end
   su.save!
+  "url=#{request.scheme}://#{request.host}/#{su.int_id.to_s(36)}"
 end
